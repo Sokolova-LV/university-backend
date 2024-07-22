302d804888517a8d5d3ef5d307419445a8feb157
Sokolova-LV <leyla-@ukr.net>
2024-07-22 09:00:12 +0300

    update

--- server.js
+++ server.js
@@ -11,29 +11,39 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const mongoURI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 5000;
+mongoose.set('strictQuery', true);
 
 // connect to MongoDB //
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose
+    .connect(mongoURI)
+    .then(() => {
+        console.log("Database connection successful");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error.message);
+        process.exit(1);
+    });
 
-const db = mongoose.connection;
+/* const db = mongoose.connection;
 db.once('open', () => {
     console.log('Connected to MongoDB');
 });
 
 db.on('error', (error) => {
     console.error('MongoDB connection error:', error);
-});
+}); */
 
 // define routes //
-// app.use('/api/lectures', require('./routes/lectures'));
-// app.use('/api/subjects', require('./routes/subjects'));
-// app.use('/api/workloads', require('./routes/workloads'));
+app.use('/api/lectures', require('./routes/lecturers'));
+app.use('/api/subjects', require('./routes/subjects'));
+app.use('/api/workloads', require('./routes/workloads'));
 
 // start server //
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+
+/* app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+}); */
